24a04ce77b40f069aaaaeb635d50c1010f397579
sjois91@gmail.com <sjois91@gmail.com>
2017-08-29 01:42:31 +0530

    Configured mongoostastic for elasticsearch

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,35 @@
 var router = require('express').Router();
 var Product = require('../models/product');
 
+Product.createMapping(function(err, mapping) {
+    if(err) {
+        console.log("error creating mapping");
+        console.log(err);
+    } else {
+        console.log("Mapping created");
+        console.log(mapping);
+    }
+});
+
+var stream = Product.synchronize();
+var count = 0;
+
+stream.on('data', function() {
+    count++;
+});
+
+stream.on('close', function() {
+    console.log("Indexed " +count + " documents");
+});
+
+stream.on('error', function(err) {
+    console.log(err);
+});
+
+router.post('/search', (req, res, next) => {
+    res.redirect('/search?q=' + req.body.q)
+});
+
 router.get('/', (req, res) => {
     res.render('main/home');
 });
