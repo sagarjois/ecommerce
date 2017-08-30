512a12043d73542aab4e3865fe8f32a789656093
sjois91@gmail.com <sjois91@gmail.com>
2017-08-30 19:43:46 +0530

    Added instant search feature on main page and other small modifications

--- api/api.js
+++ api/api.js
@@ -4,6 +4,16 @@ var faker = require('faker');
 var Category = require('../models/category');
 var Product = require('../models/product');
 
+
+router.post('/search', (req, res, next) => {
+    Product.search({
+        query_string: { query: req.body.search_term }
+    }, function(err, results) {
+        if(err) return next(err);
+        res.json(results);
+    });
+});
+
 router.get('/:name', (req, res, next) => {
     async.waterfall([
         (callback) => {
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,27 @@
 var router = require('express').Router();
 var Product = require('../models/product');
 
+function paginate(req, res, next) {
+    var perPage = 9;
+    var page = req.params.page;
+
+    Product
+        .find()
+        .skip(perPage * page)
+        .limit(perPage)
+        .populate('category')
+        .exec(function(err, products) {
+            if(err) return next(err);
+            Product.count().exec(function(err, count) {
+                if(err) return next(err);
+                res.render('main/product-main', {
+                    products: products,
+                    pages: count / perPage
+                });
+            });             
+        });
+}
+
 Product.createMapping(function(err, mapping) {
     if(err) {
         console.log("error creating mapping");
@@ -30,8 +51,33 @@ router.post('/search', (req, res, next) => {
     res.redirect('/search?q=' + req.body.q)
 });
 
-router.get('/', (req, res) => {
-    res.render('main/home');
+router.get('/search', (req, res, next) => {
+    if(req.query.q) {
+        Product.search({
+            query_string: { query: req.query.q}
+        }, function(err, results) {
+            if(err) return next(err);
+            var data = results.hits.hits.map(function(hit) {
+                return hit;
+            });
+            res.render('main/search-result', {
+                query: req.query.q,
+                data: data
+            });
+        });
+    }
+});
+
+router.get('/', (req, res, next) => {
+    if(req.user) {
+        paginate(req, res, next);
+    } else {
+        res.render('main/home');
+    }
+});
+
+router.get('/page/:page', (req, res, next) => {
+    paginate(req, res, next);
 });
 
 router.get('/about', (req, res) => {
