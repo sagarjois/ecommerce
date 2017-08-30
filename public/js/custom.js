e00cfbf0239b46c94d8316655d37f523f1d646aa
sjois91@gmail.com <sjois91@gmail.com>
2017-08-31 00:50:08 +0530

    Modified custom.js file to clear and  append html elements to populate  search result data dynamically in product-main.ejs

--- public/js/custom.js
+++ public/js/custom.js
@@ -14,7 +14,21 @@ $(function() {
                     return hit;
                 });
 
-                console.log(data);
+                $('#searchResults').empty();
+                for(var i = 0; i< data.length; i++) {
+                    var html = "";
+                    html += '<div class="col-md-4">';
+                    html += '<a href="/product/' + data[i]._source._id + '">';
+                    html += '<div class="thumbnail">';
+                    html += '<img src="' + data[i]._source.image + '">';
+                    html += '<div class="caption">';
+                    html += '<h3>' + data[i]._source.name  + '</h3>';
+                    html += '<p>' + data[i]._source.category.name  + '</p>';
+                    html += '<p>$' + data[i]._source.price  + '</p>';
+                    html += '</div></div></a></div>';
+
+                    $('#searchResults').append(html);
+                }
             },
             error: function(error) {
                 console.log(error);
